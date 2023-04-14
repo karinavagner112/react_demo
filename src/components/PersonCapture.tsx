0877d1aa9f1b243a4714e57cde5a63962c9ccc3e
karinavagner112 <wagner_97@mail.ru>
2023-04-14 16:10:48 +0200

    navigation angefangen: context erstellt, provider geschrieben

---
+++ src/components/PersonCapture.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Person } from './PersonType';
+import {
+    Grid,
+    TextField,
+    Select,
+    MenuItem,
+    InputLabel
+} from '@material-ui/core';
+import { useParams } from 'react-router-dom';
+
+
+type RoutingParams = 'id';
+
+export interface ICapturePersonProps { };
+
+export function PersonCapture(props: ICapturePersonProps) {
+    
+}
---
+++ src/components/navigationBar/NavigationBarDashboard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import clsx from 'clsx';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+} from "@material-ui/core";
+import SaveIcon from "@material-ui/icons";
+import DeleteIcon from "@material-ui/icons";
+import ReplayIcon from "@material-ui/icons";
+import useNavigateBar from "./useNavigationBar";
+
+
+
+
